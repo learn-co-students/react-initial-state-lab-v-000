dfea87f206c635d738a82683222daa1c0ba53dc4
r0tiart <antley.chu@gmail.com>
2017-09-06 13:08:03 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,22 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+ 
+class ImageSlider extends React.Component { 
+  constructor() {
+    super();
+ 
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+ 
+  render() {
+    return (
+      <slide className="toggle-slide">
+        I am on slide {this.state.currentSlideIndex}
+      </slide>
+    )
+  }
+}
+ 
+export default ImageSlider;
