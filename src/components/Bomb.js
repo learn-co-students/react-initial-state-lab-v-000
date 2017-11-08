d770848503bce9d0b3b98dc67e2f16b1577681a2
James Fernandez <jffernan@comcast.net>
2017-11-08 15:40:47 -0800

    Update Bomb.js

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default class Bomb extends React.Component {
+export default class Bomb extends Component {
   constructor(props) {
     super();
     this.state = {
