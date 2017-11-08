cef6c102e0384c1998ba9138fea6d755da0643b7
James Fernandez <jffernan@comcast.net>
2017-11-08 14:47:44 -0800

    Update Bomb.js

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default class Bomb extends React.Component {
   constructor(props) {
     super();
-
     this.state = {
       secondsLeft: props.initialCount,
     };
@@ -11,9 +10,8 @@ export default class Bomb extends React.Component {
 
   render() {
     const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
-
-    return (
-      <div>{message}</div>
-    );
+      return (
+        <div>{message}</div>
+      );
   }
 }
