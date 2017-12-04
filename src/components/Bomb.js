9d7ed77e28f7b68c1a45a329162a3a7c9ff7adc4
soheecho94 <soheecho94@gmail.com>
2017-12-04 11:18:20 +0900

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,14 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+class Bomb extends React.Component {
+  state = { secondsLeft: this.props.initialCount }
+
+  render() {
+    return (
+      <h1>{this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}</h1>
+    )
+  }
+}
+
+export default Bomb;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,23 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+class ImageSlider extends React.Component {
+  // constructor() {
+  //   super();
+  //
+  //   this.state = {
+  //     currentSlideIndex: 0
+  //   }
+  // }
+
+  state = { currentSlideIndex: 0 }
+  // can be written like this but must be used in arrow function
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+}
+
+export default ImageSlider;
