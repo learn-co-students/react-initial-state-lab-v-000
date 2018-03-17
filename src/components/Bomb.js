7a68c78f8001b41045c55e8bc13013796abdefa0
jss530 <jsifre01@gmail.com>
2018-03-16 20:32:45 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,11 +1,3 @@
-// // Bomb Component Code Goes In the components/Bomb.js file, create a Bomb React component.
-// Its initial state should have a property called secondsLeft. -done
-// The initial value of secondsLeft is set by passing in an initialCount prop to the Bomb component.
-// Don't forget to pass the argument props into the constructor (i.e., constructor(props)).
-// It should render the text '<SECONDS_LEFT> seconds left before I go boom!', where <SECONDS_LEFT>
-// is the value of secondsLeft.
-// If secondsLeft equals 0, it should render 'Boom!' instead.
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
