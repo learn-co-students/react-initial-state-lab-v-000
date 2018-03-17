cf92f163970a4ceea29ba0c202e84e86e005c140
jss530 <jsifre01@gmail.com>
2018-03-16 20:32:14 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,42 @@
-// Bomb Component Code Goes Here
+// // Bomb Component Code Goes In the components/Bomb.js file, create a Bomb React component.
+// Its initial state should have a property called secondsLeft. -done
+// The initial value of secondsLeft is set by passing in an initialCount prop to the Bomb component.
+// Don't forget to pass the argument props into the constructor (i.e., constructor(props)).
+// It should render the text '<SECONDS_LEFT> seconds left before I go boom!', where <SECONDS_LEFT>
+// is the value of secondsLeft.
+// If secondsLeft equals 0, it should render 'Boom!' instead.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const SECONDS_LEFT = parseInt(this.state.secondsLeft)
+
+    if(SECONDS_LEFT == 0) {
+      return (
+        <p>
+        Boom!
+        </p>
+      )} else {
+      return (
+      <p>
+        {SECONDS_LEFT} seconds left before I go boom!
+      </p>
+      )}
+    }
+  }
+
+Bomb.propTypes = {
+  initialCount: PropTypes.number
+};
+
+export default Bomb;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,27 @@
-// Bomb Component Code Goes Here
+// In the components/ImageSlider.js file, create an ImageSlider React component.
+// Its initial state should have a property called currentSlideIndex that starts at 0.
+// It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE>
+// is the value of this.state.currentSlideIndex.
+
+import React from 'react';
+
+class ImageSlider extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    const CURRENT_SLIDE = this.state.currentSlideIndex
+    return (
+      <p>
+        I am on slide {CURRENT_SLIDE}
+      </p>
+    )
+  }
+}
+
+export default ImageSlider;
