13a552d925717a44621815d4475ef9c90c62475b
kpeck71 <kpeck71@gmail.com>
2018-07-16 19:52:39 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -9,12 +9,16 @@ export default class Bomb extends React.Component {
     }
   }
   render() {
+    //   if (this.state.secondsLeft === 0) {
+    //     var message = 'Boom!'
+    //   } else {
+    //   var message = `${this.state.secondsLeft} seconds left before I go boom!`
+    //   }
+    // return (
+    //   <div>{message}</div>
+    // )
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
 
-      if (this.state.secondsLeft === 0) {
-        var message = 'Boom!'
-      } else {
-      var message = `${this.state.secondsLeft} seconds left before I go boom!`
-      }
     return (
       <div>{message}</div>
     )
