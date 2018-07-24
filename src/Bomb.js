114060a730d705afd53d399051f16531a230f16f
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-07-23 20:56:19 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
-// your Bomb code here!
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+  render() {
+    if (this.state.secondsLeft > 0) {
+      return (
+        <div>
+          {this.state.secondsLeft} seconds left before I go boom!
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Boom!
+        </div>
+      )
+    }
+  }
+}
+
+Bomb.propTypes = {
+  initialCount: PropTypes.number.isRequired
+}
+
+export default Bomb;
