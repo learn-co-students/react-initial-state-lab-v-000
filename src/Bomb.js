cf70a68c60687ac4809ece11ef8c2456b516e8bf
github username <github email address>
2018-08-14 06:53:55 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+// pass "props" into the state base constructor only.
+  constructor(props) {
+    super(props)
+// set the property of Bomb's "state"
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+// render in the Virtual DOM the messages I want.
+  render() {
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+}
