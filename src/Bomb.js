f2ca6d76969f13530f497a5fad6d45b36b3fe250
nmlstyl <joey.michalina.mariano@gmail.com>
2018-09-23 00:07:30 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,34 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+  constructor(prop){
+    super(prop)
+
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+  // clickDiv = () => {
+  //   this.setState({
+  //     currentSlideIndex: 0
+  //   })
+  // }
+
+  render() {
+    if (this.state.secondsLeft > 0){
+      return (
+        <div>
+            { this.state.secondsLeft } seconds left before I go boom!
+        </div>
+      )
+    } else {
+      return (
+        <div>
+            Boom!
+        </div>
+      )
+    }
+  }
+}
