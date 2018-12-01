53f1516a6dd1e9ec40e12f173661a7cb0d4e069f
Obafemi Fadairo <obafad94@gmail.com>
2018-11-30 20:11:26 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+	
+	constructor(props) {
+		super(props)
+		this.state = {
+			secondsLeft: this.props.initialCount
+		}
+	}
+
+
+// It should render the text '<SECONDS_LEFT> seconds left before I go boom!', where <SECONDS_LEFT> is the value of secondsLeft.
+	render() {
+		if (this.state.secondsLeft == 0)
+			return (
+				<div>
+					Boom!
+				</div>
+			)
+		else
+			return (
+				<div>
+				{this.state.secondsLeft} seconds left before I go boom!
+				</div>
+			)
+		end
+	}
+}
