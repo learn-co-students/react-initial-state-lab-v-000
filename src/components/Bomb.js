30e55af8726e6b38f14af621346588e331302146
Joanna Sese <jmgsese@gmail.com>
2019-01-10 21:35:51 -0600

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class Bomb extends React.Component {
-  constructor(props) {
-    super(props);
-
+export default class Bomb extends Component {
+  constructor(props){
+    super()
     this.state = {
       secondsLeft: props.initialCount
     }
   }
 
-  render() {
-    if (this.state.secondsLeft === 0){
-      return 'Boom!'
-    } else {
-      return  `${this.state.secondsLeft} seconds left before I go boom!`
-    }
+  render(){
+    const seconds = this.state.secondsLeft;
+    const bombMemo = seconds > 0 ? `${seconds} seconds left before I go boom!` : `Boom!`
+    return(
+      <div>
+        {bombMemo}
+      </div>
+    )
   }
 }
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class ImageSlider extends React.Component {
+export default class ImageSlider extends Component {
   constructor(){
-    super();
-
-      this.state = {
-        currentSlideIndex: 0
-      }
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
   }
 
-  render() {
-    return (
-      `I am on slide ${this.state.currentSlideIndex}`
+  render(){
+    return(
+      <div>I am on slide {this.state.currentSlideIndex}</div>
     )
   }
 }
