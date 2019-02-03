2be0959316f4a13e7cae43e95102ff2760986c30
Kyle Hughes <blinded93@gmail.com>
2019-02-02 19:14:55 -0800

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -7,12 +7,20 @@ export default class Bomb extends Component {
             secondsLeft:props.initialCount
         }   
     }
+    
+    timer = () => {
+        setInterval(() => {
+            this.setState({
+                secondsLeft: this.state.secondsLeft - 1
+            })
+        }, 1000)
+    }
 
     render() {
         if (this.state.secondsLeft > 0) {
-            return (<div className="timer">{this.state.secondsLeft} seconds left before I go boom!</div>)
+            return (<div className="timer" onClick={this.timer}>{this.state.secondsLeft} seconds left before I go boom!</div>)
         } else {
-            return (<div className="timer">Boom!</div>)
+            return (<div className="timer" onClick={this.timer}>Boom!</div>)
         }
     }
 }
