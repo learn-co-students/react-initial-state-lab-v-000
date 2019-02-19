b21b65c56985ed7d7e2af4c82279475cb594c660
github username <github email address>
2019-02-19 06:32:37 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+      super(props)
+
+      this.state = {
+       initialCount: this.props.value,
+      }
+    }
+    //
+    // handleClick = () => {
+    //   this.setState({
+    //     color: '#333'
+    //   })
+    // }
+
+    render() {
+      return (
+        <div className="">
+      //  style={{backgroundColor: this.state.color}}
+      //   onClick={this.handleClick}>
+        </div>
+      )
+    }
+  }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,29 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+
+    constructor(props) {
+      super(props)
+
+      this.state = {
+    //   initialCount: this.props.value,
+      }
+    }
+    //
+    // handleClick = () => {
+    //   this.setState({
+    //     color: '#333'
+    //   })
+    // }
+
+    render() {
+      return (
+        <div className="">
+      //  style={{backgroundColor: this.state.color}}
+      //   onClick={this.handleClick}>
+        </div>
+      )
+    }
+  }
