1dbeeb5a5422b60e64e72935a30686168adbb8a8
Rachel Wyatt <rw126@evansville.edu>
2019-07-03 18:15:30 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class Bomb extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {secondsLeft: props.initialCount};
+    } 
 
     render() {
-      return "TBD";
+    if (this.state.secondsLeft === 0){
+        return ("Boom!")
+    }
+    else {
+      return (this.state.secondsLeft + " seconds left before I go boom!");
+    }
     }
 }
 
