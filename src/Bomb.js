b7e9a3e42bcdb03fc45d30121e3510f9c1e90f17
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-09-15 01:13:16 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+
+export default class Bomb extends React.Component {
+
+	constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+ 
+
+    timesUp = () => { 
+    this.setState({
+      secondsLeft: 0
+    })
+   } 
+
+	render() {
+	 if(this.state.secondsLeft === 0) {
+      return (
+         <div>
+          Boom!
+        </div>
+      );
+  } else {
+  	  return (
+  	<div>{this.state.secondsLeft} seconds left before I go boom!</div>
+  );}
+	}
+}
