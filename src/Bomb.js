9ddb98b5fa39b83a052b60d765a821c5964aa9ec
Conrad Bituin <38743840+cbituin@users.noreply.github.com>
2019-10-08 10:05:21 -0500

    Complete

--- src/Bomb.js
+++ src/Bomb.js
@@ -4,9 +4,17 @@ import React, { Component } from "react";
 export default class Bomb extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      secondsLeft: this.props.initialCount
+    };
   }
 
   render() {
-    return <div />;
+    const timer =
+      this.state.secondsLeft === 0
+        ? "Boom!"
+        : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return <div>{timer}</div>;
   }
 }
