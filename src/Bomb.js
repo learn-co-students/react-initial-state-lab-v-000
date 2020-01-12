06a9c220450812a29090685320616fb673403d10
yuanxizhang <yuanxi.zhang@gmail.com>
2020-01-11 17:02:37 -0800

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -6,6 +6,24 @@ class Bomb extends React.Component {
     this.state = {secondsLeft: this.props.initialCount};
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    ); 
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+  	const n = this.state.secondsLeft > 0 ?  this.state.secondsLeft : 1;
+    this.setState({
+      secondsLeft:  n - 1 
+    });
+  }
+
   render() {
   	const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
 	  return (    
