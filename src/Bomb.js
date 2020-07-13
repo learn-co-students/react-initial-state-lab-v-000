7c09eee350158d77b24f6e5a81bf7ab24ed02e44
github username <github email address>
2020-07-13 12:06:28 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,20 +1,35 @@
 // your Bomb code here!
-import React from 'react';
-// import React, { Component } from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 
 
 export default class Bomb extends Component {
   constructor(props) {
     super()
     this.state = {
-      count: 0
+      secondsLeft: props.initialCount
     }
   }
 
 
   render() {
+    // let message;
+    // console.log("message", message)
+    // console.log("secondsLeft", this.state.secondsLeft)
+    //
+    // if (this.state.secondsLeft == 0) {
+    //   let message = 'Boom!'
+    //   console.log("message2", message)
+    // } else {
+    //   console.log(message)
+    //
+    //   let message = '120 left before I go boom!'
+    // }
+
     return (
-      <div>hi</div>
+      <div>
+        { this.state.secondsLeft == 0 ? 'Boom!' : '120 seconds left before I go boom!' }
+      </div>
     )
   }
 }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,14 +1,20 @@
 // your ImageSlider code here!
-
 import React from 'react';
 // import React, { Component } from 'react';
 
 
-export default class ImageSlider extends Component {
+export default class ImageSlider extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {currentSlideIndex: 0}
+  }
+
 
   render() {
     return (
-      <div> hi</div>
+      // console.log(this.state.currentSlideIndex)
+      <div>I am on slide { this.state.currentSlideIndex }</div>
     )
   }
 };
