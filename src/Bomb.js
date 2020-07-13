e3b16e67cb324b62d5546389af54c9d2a0e7a956
github username <github email address>
2020-07-13 11:13:42 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
 // your Bomb code here!
+import React from 'react';
+// import React, { Component } from 'react';
+
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,14 @@
 // your ImageSlider code here!
+
+import React from 'react';
+// import React, { Component } from 'react';
+
+
+export default class ImageSlider extends Component {
+
+  render() {
+    return (
+      <div> hi</div>
+    )
+  }
+};
