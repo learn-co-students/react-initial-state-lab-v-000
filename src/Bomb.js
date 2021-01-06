fcc6ccdb2b75a84633a2859a99edb6a65646e821
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-05 23:40:11 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,8 @@
 // your Bomb code here!
+import React, {Component} from 'react';
+
+class Bomb extends Component {
+
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,8 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+class ImageSlider extends Component {
+
+}
+
+export default ImageSlider
