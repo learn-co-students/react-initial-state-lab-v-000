4c6708bd06ba8029e44b32b0688d57846327d617
emanao <emanao@me.com>
2021-05-11 13:33:58 -0400

    Set initial state: secondsLeft to initialCount

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,6 +1,13 @@
 import React, { Component }  from 'react';
 // your Bomb code here!
+
 export default class Bomb extends React.Component{
+    constructor(props){
+        super();
+        this.state={
+            secondsLeft: props.initialCount
+        }
+    }
     render(){
         return(
             <div></div>
